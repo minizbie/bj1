de111048c0e5448b7ea893507dd0f545489736ee
Lawrence Wong <lawrencewong@gmail.com>
2019-03-04 17:09:47 +0800

    commit 20190304

--- src/main/kotlin/bj1/Card.kt
+++ src/main/kotlin/bj1/Card.kt
@@ -1,3 +1,31 @@
 package bj1
 
-class Card( val value:Int, val suit:Int)
+import java.lang.IllegalStateException
+
+class Card(val value:Int, val suit:Int){
+
+    val suitName:String get() = when (suit){
+        1->"Spades"
+        2->"Hearts"
+        3->"Clubs"
+        4->"Diamonds"
+        else->throw IllegalStateException()
+    }
+
+    val valueName:String get() = when (value){
+        1->"Ace"
+        2->"Two"
+        3->"Three"
+        4->"Four"
+        5->"Five"
+        6->"Six"
+        7->"Seven"
+        8->"Eight"
+        9->"Nine"
+        10->"Ten"
+        11->"Jack"
+        12->"Queen"
+        13->"King"
+        else->throw IllegalStateException()
+    }
+}
